bb9ca3a6007eddba4dc2130c5c4354b0c6fb68ff
matheobrise <matheo.brise17@gmail.com>
2022-06-29 15:31:56 +0200

    joi

--- backend_mongo/src/routes/article.ts
+++ backend_mongo/src/routes/article.ts
@@ -2,18 +2,8 @@ import express from 'express';
 import controller from '../controllers/article';
 
 const router = express.Router();
-import multer from 'multer';
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'uploads');
-  },
-  filename: (req, file, cb) => {
-    cb(null, file.fieldname + '-' + Date.now());
-  },
-});
 
-const upload = multer({ storage: storage });
-router.post('/create', upload.single('image'), controller.createArticle);
+router.post('/create', controller.createArticle);
 router.get('/', controller.getAllArticles);
 router.get('/:id', controller.getOneArticle);
 router.put('/:id', controller.updateArticle);
